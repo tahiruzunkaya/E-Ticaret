d9b6fbf2f03214d2903bd1806ea3bd7bc4e1a58c
Tahir.Uzunkaya <Tahir.Uzunkaya@TAHIRUZUNKAYA>
2019-08-08 13:58:45 +0300

    testtest

--- E-Ticaret.WebUI/Controllers/HomeController.cs
+++ E-Ticaret.WebUI/Controllers/HomeController.cs
@@ -32,7 +32,7 @@ namespace ETicaret.WebUI.Controllers
 
         public IActionResult AboutUs()
         {
-            // tahiruzunkaya 1
+            // tahiruzunkaya 1 2
             return View();
         }
         public IActionResult Categories(int id)
